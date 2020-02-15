8b7ba1ba8454a11adda0f7e96a9af8fd6131705e
andresvelazq <andres.a.velazquez@gmail.com>
2020-02-15 15:17:55 -0500

    event adder

--- Schedulo/app/src/main/java/com/example/schedulo/MainActivity.java
+++ Schedulo/app/src/main/java/com/example/schedulo/MainActivity.java
@@ -1,8 +1,10 @@
 package com.example.schedulo;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void AddCalendarEvent(View view) {
+        Calendar calendarEvent = Calendar.getInstance();
+        Intent intent = new Intent(Intent.ACTION_EDIT);
+        intent.setType("vnd.android.cursor.item/event");
+        intent.putExtra("beginTime", calendarEvent.getTimeInMillis());
+        intent.putExtra("allDay", true);
+        intent.putExtra("rule", "FREQ=YEARLY");
+        intent.putExtra("endTime", calendarEvent.getTimeInMillis() + 60 * 60 * 1000);
+        intent.putExtra("title", "Calendar Event");
+        startActivity(intent);
     }
 }
