16a773d84c3cafb9fea4fffb816a1f66a5657767
sam-sam98 <60492280+sam-sam98@users.noreply.github.com>
2020-02-16 19:52:16 -0500

    Update Register.java

--- Schedulo/app/src/main/java/com/example/schedulo/Register.java
+++ Schedulo/app/src/main/java/com/example/schedulo/Register.java
@@ -2,13 +2,76 @@ package com.example.schedulo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.google.gson.Gson;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class Register extends AppCompatActivity {
+    private static final String SERVER_URL = "http://localhost:7000";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+    }
+
+    public void registerUser(View view) {
+        // Collect log in info
+        EditText getName = findViewById(R.id.name);
+        EditText getPhone = findViewById(R.id.number);
+        EditText getPass = findViewById(R.id.password);
+
+        final String inputName = getName.getText().toString();
+        final String inputPhone = getPhone.getText().toString();
+        final String inputPass = getPass.getText().toString();
+
+        // Serialize
+        User newUser = new User(inputName, inputPhone, inputPass);
+        Gson gson = new Gson();
+        final String userString = gson.toJson(newUser);
+
+        // Post to server
+        final TextView textView = findViewById(R.id.textView);
+        RequestQueue queue = Volley.newRequestQueue(this);
+        final String registerURL = SERVER_URL + "/register";
+        StringRequest postRequest = new StringRequest(Request.Method.POST, registerURL,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        System.out.println("\n\nSUCCESS\n\n" + response);
+                        Intent intent = new Intent(Register.this, MainActivity.class);
+                        Register.this.startActivity(intent);
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        System.out.println("\n\nERROR\n\n" + error.toString());
+                        textView.setText("Could not complete request at this time.");
+                    }
+                }) {
+            @Override
+            protected Map<String, String> getParams() {
+                Map<String, String> params = new HashMap<String, String>();
+                params.put("username", inputPhone);
+                params.put("password", inputPass);
+
+                return params;
+            }
+        };
+        queue.add(postRequest);
     }
 }
