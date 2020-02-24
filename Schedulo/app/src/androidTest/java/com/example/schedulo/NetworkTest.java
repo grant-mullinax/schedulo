c69f28da89a080b27a1365da1757c251dba5ab5d
Andy Phan <andytphan@outlook.com>
2020-02-23 19:25:11 -0500

    renamed test

--- Schedulo/app/src/androidTest/java/com/example/schedulo/NetworkTest.java
+++ Schedulo/app/src/androidTest/java/com/example/schedulo/NetworkTest.java
@@ -30,7 +30,7 @@ import static org.junit.Assert.*;
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 @RunWith(AndroidJUnit4.class)
-public class ExampleInstrumentedTest {
+public class NetworkTest {
 
     private String username, password, wrongPassword;
 
