70f49d8e026339d228b00db1eaea6d661afeb41b
Andy Phan <andytphan@outlook.com>
2020-03-08 12:53:48 -0400

    view events

--- Schedulo/app/src/main/java/com/example/schedulo/ViewEvents.java
+++ Schedulo/app/src/main/java/com/example/schedulo/ViewEvents.java
@@ -8,21 +8,44 @@ import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class ViewEvents extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_events);
-        CalendarEvent[] events = MainActivity.events.toArray(new CalendarEvent[MainActivity.events.size()]);
-        ArrayAdapter<CalendarEvent> adapter = new ArrayAdapter<CalendarEvent>(this,
+        CalendarEvent[] events = setupEvents(MainActivity.events);
+        CalendarAdapter adapter = new CalendarAdapter(this,
                 android.R.layout.simple_list_item_1, events);
         ListView listView = (ListView) findViewById(R.id.eventList);
         listView.setAdapter(adapter);
     }
 
+    public CalendarEvent[] setupEvents(List<CalendarEvent> events) {
+        Collections.sort(events);
+        List<CalendarEvent> returns = new ArrayList<>();
+        String prev = "";
+        for(CalendarEvent e : events) {
+            if(e.getDate().compareTo(prev) != 0) {
+                returns.add(new CalendarEvent(e.getDate(), "", "", -1, -1));
+                prev = e.getDate();
+            }
+            returns.add(e);
+        }
+        return returns.toArray(new CalendarEvent[returns.size()]);
+    }
+
     public void Back(View view) {
         Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+
+    public void editEvent(View view) {
+        Intent intent = new Intent(this, EditCalendarEvent.class);
         startActivity(intent);
     }
 }
